2cd9b5a591db6234224d93a1bd6e2c48bd0848fa
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-06-17 11:30:04 +0100

    fix some bugs on the following redirection : < and >

--- run/redirect.c
+++ run/redirect.c
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 21:03:03 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/06/16 22:21:39 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/06/17 11:27:10 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void redirect(int is_path, t_parsing *shell, char **content) {
   int last_fd_readed = 0;
   if (is_path == 1) {
     int fd = 0;
+    int fd2 = 0;
     if (shell->n_in_files >= 1 || shell->n_out_files >= 1) {
       int count = 0;
       while (content[count]) {
@@ -30,23 +31,23 @@ void redirect(int is_path, t_parsing *shell, char **content) {
           dup2(fd, 1);
           last_fd_readed = 1;
         }else if (!ft_strncmp(content[count], "<", ft_strlen("<"))) {
-          fd = open(content[count + 1], O_RDONLY);
-          if (fd  < 0) {
+          fd2 = open(content[count + 1], O_RDONLY);
+          if (fd2  < 0) {
             dup2(2, 1);
             printf("minishell: %s: No such file or directory\n", content[count + 1]);
             last_fd_readed = 1;
             exit(127);
           }
-          dup2(fd, 0);
+          dup2(fd2, 0);
           last_fd_readed = 0;
         }
         count++;
       }
     }
     if (last_fd_readed == 0)
-      dup2(fd, 1);
+      dup2(fd2, 0);
     else if (last_fd_readed == 1)
-      dup2(fd, 0);
+      dup2(fd, 1);
     if (execve(shell->cmd, join_two_dim_arr(shell->cmd + ft_strlen("/bin/"), new_content(content)), NULL) == -1) {
       printf("minishell: %s: command not found\n", shell->cmd);
       exit(127);
@@ -83,9 +84,9 @@ void redirect(int is_path, t_parsing *shell, char **content) {
       }
     }
     if (last_fd_readed == 0)
-      dup2(fd, 1);
-    else if (last_fd_readed == 1)
       dup2(fd2, 0);
+    else if (last_fd_readed == 1)
+      dup2(fd, 1);
     if (execve(ft_strjoin("/bin/", shell->cmd), join_two_dim_arr(shell->cmd, new_content(content)), NULL) == -1) {
       printf("minishell: %s: command not found\n", shell->cmd);
       exit(127);
