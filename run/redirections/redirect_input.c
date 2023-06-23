1e31e9077d9981ac7743be6b41e8067201bca81e
Mohamed Amine Amazzal <mamazzal@c2r4p10.1337.ma>
2023-06-23 20:14:50 +0100

    init redirect output

--- run/redirections/redirect_input.c
+++ run/redirections/redirect_input.c
@@ -6,14 +6,21 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 22:45:05 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/06/23 01:41:15 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/06/23 20:11:26 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
 void redirect_input(char **content, int count) {
-  (void)count;
-  (void)content;
-  //todo : create input redirection
+   if (access(content[count + 1], F_OK) == -1) {
+      dup2(0, 1);
+      ft_putstr_fd("minishell: ", 2);
+      ft_putstr_fd(content[count + 1], 2);
+      ft_putstr_fd(": No such file or directory\n", 2);
+      close(1);
+      return;
+   }
+  int fd = open(content[count + 1], O_RDONLY);
+  dup2(fd, 0);
 }
