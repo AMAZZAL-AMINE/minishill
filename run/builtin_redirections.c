12cd907da5d6310757fa6fe36e66a7bf531709b2
Mohamed Amine Amazzal <mamazzal@c2r1p5.1337.ma>
2023-07-21 21:58:18 +0100

    init redirection of builtin

--- run/builtin_redirections.c
+++ run/builtin_redirections.c
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 10:40:31 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/07/21 13:14:22 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/07/21 21:46:31 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,54 @@ int	is_dolar_(char *str)
 	return (0);
 }
 
+char	**get_new_args(char **content, t_minishell *mini)
+{
+	int		count;
+	char	**new_content;
+
+	count = 0;
+	new_content = duplicate_content(content);
+	count = 0;
+	while (new_content[count])
+	{
+		if (is_dolar_(new_content[count]) \
+			&& is_bettwen_double(new_content[count]))
+			new_content[count] = expand(new_content[count], mini);
+		new_content[count] = remove_quots(new_content[count]);
+		count++;
+	}
+	return (new_content);
+}
+
+int	run_redirections(char **content, t_minishell *mini, \
+	int count, char **new_content)
+{
+	int	captur;
+
+	captur = 0;
+	if (str_cmp(">", content[count]))
+	{
+		captur = check_redrect_output(new_content, mini, count, \
+			content[count + 1]);
+		if (captur != 0)
+			return (captur);
+	}
+	if (str_cmp("<", content[count]))
+	{
+		captur = check_redrect_input(new_content, mini, count, \
+			content[count + 1]);
+		if (captur != 0)
+			return (captur);
+	}
+	if (str_cmp(">>", content[count]))
+	{
+		captur = check_appned(mini, content[count + 1], count, new_content);
+		if (captur != 0)
+			return (captur);
+	}
+	return (0);
+}
+
 int	builtin_redirections(char **content, t_parsing *shell, t_minishell *mini)
 {
 	int		captur;
@@ -38,65 +86,13 @@ int	builtin_redirections(char **content, t_parsing *shell, t_minishell *mini)
 		herdoc(content, mini);
 	if (is_redirec_output(content))
 	{
-		count = 0;
-		new_content = duplicate_content(content);
-		count = 0;
-		while (new_content[count])
-		{
-			if (is_dolar_(new_content[count]) \
-				&& is_bettwen_double(new_content[count]))
-				new_content[count] = expand(new_content[count], mini);
-			new_content[count] = remove_quots(new_content[count]);
-			count++;
-		}
+		new_content = get_new_args(content, mini);
 		count = 0;
 		while (content[count])
 		{
-			if (str_cmp(">", content[count]))
-			{
-				if (content[count + 1] \
-					&& !is_ambiguous_file(content[count + 1], mini))
-				{
-					captur = redirect_output(new_content, count);
-					if (captur != 0)
-						break ;
-				}
-				else
-				{
-					captur = 1;
-					break ;
-				}
-			}
-			else if (str_cmp("<", content[count]))
-			{
-				if (content[count + 1] \
-					&& !is_ambiguous_file(content[count + 1], mini))
-				{
-					captur = redirect_input(new_content, count);
-					if (captur != 0)
-						break ;
-				}
-				else
-				{
-					captur = 1;
-					break ;
-				}
-			}
-			else if (str_cmp(">>", content[count]))
-			{
-				if (content[count + 1] \
-					&& !is_ambiguous_file(content[count + 1], mini))
-				{
-					captur = appned(new_content, count);
-					if (captur != 0)
-						break ;
-				}
-				else
-				{
-					captur = 1;
-					break ;
-				}
-			}
+			captur = run_redirections(content, mini, count, new_content);
+			if (captur == 1)
+				break ;
 			count++;
 		}
 		shell->args = get_args_without_redirections(shell->args);
