621c43c17c62246ffbb6c42e6a80af84c4bf69e8
Mohamed Amine Amazzal <mamazzal@c1r3p7.1337.ma>
2023-07-25 18:56:52 +0100

    init one free in export_utilis_update_exported_var.cat line 34

--- run/utilis/export_utilis_update_exported_var.c
+++ run/utilis/export_utilis_update_exported_var.c
@@ -6,7 +6,7 @@
 /*   By: mamazzal <mamazzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 10:22:07 by mamazzal          #+#    #+#             */
-/*   Updated: 2023/07/25 18:45:50 by mamazzal         ###   ########.fr       */
+/*   Updated: 2023/07/25 18:54:15 by mamazzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	update_exported_var(char *content, \
 {
 	int		count;
 	char	**splited;
+	char	*tmp;
 
 	count = 0;
 	while (count < shell->n_var_env)
@@ -28,7 +29,9 @@ void	update_exported_var(char *content, \
 		{
 			if (ft_strchr(content, '=') != NULL)
 			{
-				shell->env_v[count] = ft_strjoin(ft_strjoin(name, "="), value);
+				tmp = ft_strjoin(name, "=");
+				shell->env_v[count] = ft_strjoin(tmp, value);
+				free(tmp);
 				free_double(splited);
 				return ;
 			}
